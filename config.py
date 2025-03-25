9c4cfba6fda1869389936495d4e2b240b6131fe5
Livia Lehmann <71011881+livleh@users.noreply.github.com>
2025-03-25 13:27:42 +0100

    changed subscriptions and config back

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 # Set your GH Pages URL here e.g.:
-ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
-ghRawURL = "https://raw.githubusercontent.com/conoro/tiktok-rss-flat/main/"
+
+ghRawURL = "https://raw.githubusercontent.com/livleh/tiktok-rss-flat/main/"
+
+ghPagesURL = "https://livleh.github.io/tiktok-rss-flat/"
 
 # ghPagesURL = "https://tiktokrss.conoroneill.com/"
